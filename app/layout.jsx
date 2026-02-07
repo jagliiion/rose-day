45909a46dfd2524008308d020705c757d8575658
jagliiion <aravindnaidu2370@gmail.com>
2026-02-07 12:43:48 +0530

    Add global styles and metadata for RootLayout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,1 +1,13 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "Rose Day Special",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
